387b91e70da41244cc47898e35f3f09314a55765
Shaik Amaan <shaikamaanahmed2585@gmail.com>
2025-02-14 10:28:10 +0530

    done: till transformer-block

---
+++ model.py
@@ -0,0 +1,139 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import tiktoken
+
+class GPTDatasetV1(Dataset):
+    def __init__(self,text,tokenizer,max_length,stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        token_ids = tokenizer.encode(text)
+
+        for i in range(0,len(token_ids) - max_length,stride):
+            input_chunks = token_ids[i:i+max_length]
+            target_chunks = token_ids[i + 1:i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunks))
+            self.target_ids.append(torch.tensor(target_chunks))
+    
+    def __len__(self):
+        return len(self.input_ids)
+    
+    def __getitem__(self,idx):
+        return self.input_ids[idx],self.target_ids[idx]
+    
+class MultiHeadAttention(nn.Module):
+    def __init__(self,d_in,d_out,context_len,dropout,num_heads,qkv_bias=False):
+        super.__init__()
+        assert (d_out % num_heads == 0) , "d_out must be divisible by num_heads"
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads
+
+        self.W_query = nn.Linear(d_in,d_out,qkv_bias)
+        self.W_key = nn.Linear(d_in,d_out,qkv_bias)
+        self.W_value = nn.Linear(d_in,d_out,qkv_bias)
+
+        self.out_proj = nn.Linear(d_out,d_out)
+        self.dropout = dropout
+
+        self.register_buffer(
+            "mask",
+            torch.triu(torch.ones(context_len,context_len),diagonal=1)
+        )
+
+    def forward(self,x):
+        b,num_tokens,d_in = x.shape
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        keys = keys.view(b,num_tokens,self.num_heads,self.head_dim)
+        values = values.view(b,num_tokens,self.num_heads,self.head_dim)
+        queries = queries.view(b,num_tokens,self.num_heads,self.head_dim)
+
+        keys= keys.transpose(1,2)
+        values = values.transpose(1,2)
+        queries = queries.transpose(1,2)
+
+        attention_scores = keys @ queries.transpose(2,3)
+
+        mask_bool = self.mask.bool()[:num_tokens,:num_tokens]
+
+        attention_scores.masked_fill_(mask_bool,-torch.inf)
+
+        attention_weights = torch.softmax(attention_scores / keys.shape[-1]**0.5,dim=-1)
+
+        attention_weights = self.dropout(attention_weights)
+
+        context_vec = (attention_weights @ values).transpose(1,2)
+
+        context_vec = context_vec.contiguous().view(b,num_tokens,self.d_out)
+
+        context_vec = self.out_proj(context_vec)
+
+        return context_vec
+
+
+class LayerNorm(nn.Module):
+    def __init__(self,emb_dim):
+        super.__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self,x):
+        mean = x.mean(keepdim=True, dim=-1,unbiased=False)
+        variance = x.var(keepdim=True,dim=-1, unbiased = False)
+        x_norm = (x - mean) / torch.sqrt(variance + self.eps)
+        return self.scale * x_norm + self.shift
+    
+class GELU(nn.Module):
+    def __init__(self):
+        super.__init__()
+    
+    def forward(self,x):
+        return 0.5 * x * (1 + torch.tanh(torch.sqrt(torch.tensor(2.0/torch.pi))* (x + 0.044715 * torch.pow(x,3))))
+
+class FeedForward(nn.Module):
+    def __init__(self,cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"],4*cfg["emb_dim"]),
+            GELU(),
+            nn.Linear(4 * cfg["emb_dim"],cfg["emb_dim"])
+        )
+    
+    def forward(self,x):
+        return self.layers(x)
+    
+class TransformerBlock(nn.Module):
+    def __init__(self,cfg):
+        super.__init__()
+        self.att = MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_len=cfg["context_len"],
+            num_heads=cfg["n_heads"],
+            dropout=cfg["drop_rate"],
+            qkv_bias=cfg["qkv_bias"]
+        )
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+
+    def forward(self,x):
+
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+
+        shortcut = x
+        x = self.norm2
+        x = self.att(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+        return x
